eb779c074f29708edba1fcb067bfa7b9da57707a
Takeru Ohta <phjgt308@gmail.com>
2015-05-11 10:16:29 +0900

    Add heaps_bench module

--- src/heaps_bench.erl
+++ src/heaps_bench.erl
@@ -9,14 +9,18 @@
 %%-export([do_bench/0, print_bench/0]).
 -export([
          bench_in/1,
-         bench_out/1
+         bench_out/1,
+         bench_in_out/1,
+         bench_merge/1
         ]).
 
 %%----------------------------------------------------------------------------------------------------------------------
-%% Types
+%% Types & Macros
 %%----------------------------------------------------------------------------------------------------------------------
 -type microseconds() :: number().
 -type average_elapsed_time() :: microseconds().
+
+-define(LOOP_COUNT, 3).
 
 %%----------------------------------------------------------------------------------------------------------------------
 %% Exported Functions
@@ -29,6 +33,16 @@ bench_in(Items) ->
 bench_out(Items) ->
     [{Module, bench_out(Module, Items)} || Module <- modules()].
 
+-spec bench_in_out([Spec]) -> [{module(), average_elapsed_time()}] when
+      Spec :: {in, heaps:item()} | out.
+bench_in_out(Specs) ->
+    [{Module, bench_in_out(Module, Specs)} || Module <- modules()].
+
+-spec bench_merge([Pair]) -> [{module(), average_elapsed_time()}] when
+      Pair :: {[heaps:item()], [heaps:item()]}.
+bench_merge(Pairs) ->
+    [{Module, bench_merge(Module, Pairs)} || Module <- modules()].
+
 %%----------------------------------------------------------------------------------------------------------------------
 %% Internal Functions
 %%----------------------------------------------------------------------------------------------------------------------
@@ -38,8 +52,16 @@ modules() -> [leftist_heaps, pairing_heaps, splay_heaps, gb_set_heaps, list_heap
 -spec bench_in(module(), [heaps:item()]) -> average_elapsed_time().
 bench_in(Module, Items) ->
     erlang:garbage_collect(),
-    Elapsed = times(3, fun () -> from_list(Module, Items) end),
-    Elapsed / 3 / length(Items).
+    Elapsed = times(?LOOP_COUNT, fun () -> from_list(Module, Items) end),
+    Elapsed / ?LOOP_COUNT / length(Items).
+
+-spec bench_merge(module(), [Pair]) -> average_elapsed_time() when
+      Pair :: {[heaps:item()], [heaps:item()]}.
+bench_merge(Module, Pairs) ->
+    HeapPairs = [{from_list(Module, A), from_list(Module, B)} || {A, B} <- Pairs],
+    erlang:garbage_collect(),
+    Elapsed = times(?LOOP_COUNT, fun () -> lists:foreach(fun ({A, B}) -> Module:merge(A, B) end, HeapPairs) end),
+    Elapsed / ?LOOP_COUNT / length(Pairs).
 
 -spec bench_out(module(), [heaps:item()]) -> average_elapsed_time().
 bench_out(Module, Items) ->
@@ -47,8 +69,25 @@ bench_out(Module, Items) ->
     true = (length(Items) =:= size(Module, Heap)),
 
     erlang:garbage_collect(),
-    Elapsed = times(3, fun () -> out_all(Module, Heap) end),
-    Elapsed / 3 / length(Items).
+    Elapsed = times(?LOOP_COUNT, fun () -> out_all(Module, Heap) end),
+    Elapsed / ?LOOP_COUNT / length(Items).
+
+-spec bench_in_out(module(), [Spec]) -> average_elapsed_time() when
+      Spec  :: {in, heaps:item()} | out.
+bench_in_out(Module, Specs) ->
+    Heap0 = Module:new(),
+    erlang:garbage_collect(),
+    Elapsed = times(?LOOP_COUNT, fun () -> in_out_all(Module, Heap0, Specs) end),
+    Elapsed / ?LOOP_COUNT / length(Specs).
+
+-spec in_out_all(module(), heaps:heap(), [Spec]) -> ok when
+      Spec  :: {in, heaps:item()} | out.
+in_out_all(_, _, [])                     -> ok;
+in_out_all(Module, Heap, [Spec | Specs]) ->
+    case Spec of
+        out        -> in_out_all(Module, element(2, Module:out(Heap)), Specs);
+        {in, Item} -> in_out_all(Module, Module:in(Item, Heap), Specs)
+    end.
 
 -spec from_list(module(), [heaps:item()]) -> heaps:heap().
 from_list(Module, Items) ->
