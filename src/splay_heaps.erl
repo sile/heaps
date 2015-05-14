95f63dbb5fbbc7e6f24bf0b9065c1278f2ba1630
Takeru Ohta <phjgt308@gmail.com>
2015-05-14 16:32:29 +0900

    Change the implementation of splay_heaps module

--- src/splay_heaps.erl
+++ src/splay_heaps.erl
@@ -17,13 +17,9 @@
 %%----------------------------------------------------------------------------------------------------------------------
 %% Types
 %%----------------------------------------------------------------------------------------------------------------------
--opaque heap(Item) :: maybe_tree_node(Item).
+-opaque heap(Item) :: empty | {heap(Item), Item, heap(Item)}.
 -type heap() :: heap(item()).
 -type item() :: term().
-
--type maybe_tree_node(Item) :: tree_node(Item) | nil.
--type tree_node(Item)       :: {Item, maybe_tree_node(Item), maybe_tree_node(Item)}.
--type direction()           :: lft | rgt.
 
 %%----------------------------------------------------------------------------------------------------------------------
 %% Exported Functions
@@ -31,101 +27,66 @@
 
 %% @doc Returns an empty heap
 -spec new() -> heap().
-new() -> nil.
+new() -> empty.
 
 %% @doc Tests if `Heap' is empty and returns `true' if so and `false' otherwise
 -spec is_empty(Heap :: heap()) -> boolean().
-is_empty(nil) -> true;
-is_empty(_)   -> false.
+is_empty(empty) -> true;
+is_empty(_)     -> false.
 
 %% @doc Inserts `Item' into the heap `Heap'
 %%
 %% Returns the resulting heap
 -spec in(Item, Heap :: heap(Item)) -> heap(Item).
 in(Item, Heap) ->
-    splay(leaf(Item), path_to_node(Item, Heap, [])).
+    {Left, Right} = partition(Item, Heap),
+    {Left, Item, Right}.
 
 %% @doc Removes the smallest item from the heap `Heap'
 %%
 %% Returns the tuple `{{value, Item}, Heap2}', where `Item' is the item removed and `Heap2' is the resulting heap.
 %% If `Heap' is empty, the tuple `{empty, Heap}' is returned.
 -spec out(Heap :: heap(Item)) -> {{value, Item}, Heap2 :: heap(Item)} | {empty, Heap :: heap(Item)}.
-out(nil)              -> {empty, nil};
-out({Item, nil, Rgt}) -> {{value, Item}, Rgt};
-out(Heap)             -> out(lft(Heap), [Heap]).
+out(empty) -> {empty, empty};
+out(Heap0) ->
+    {X, Heap1} = out_impl(Heap0),
+    {{value, X}, Heap1}.
 
 %% @doc Returns the merged heap of `Heap1' and `Heap2'
 -spec merge(Heap1 :: heap(Item1), Heap2 :: heap(Item2)) -> heap(Item1|Item2).
-merge(Heap1, Heap2) -> fold(fun in/2, Heap1, Heap2).
+merge(empty,     Heap)  -> Heap;
+merge({L1, X, R1}, Heap) ->
+    {L2, R2} = partition(X, Heap),
+    {merge(L2, L1), X, merge(R2, R1)}.
 
 %% @doc Folds `Function' over every item in `Heap' returing the final value of the accumulator
 -spec fold(heaps:fold_fun(Item), term(), heap(Item)) -> term().
-fold(Fun, Acc0, Tree) -> fold_node(Fun, Tree, Acc0).
+fold(_Fun, Acc, empty)    -> Acc;
+fold(Fun, Acc, {A, X, B}) -> fold(Fun, fold(Fun, Fun(X, Acc), A), B).
 
 %%--------------------------------------------------------------------------------
 %% Internal Functions
 %%--------------------------------------------------------------------------------
--spec item(tree_node(Item)) -> Item.
-item({Item, _, _}) -> Item.
+-spec out_impl(heap(Item)) -> {Item, heap(Item)}.
+out_impl({empty,         Y, C}) -> {Y, C};
+out_impl({{empty, X, B}, Y, C}) -> {X, {B, Y, C}};
+out_impl({{    A, X, B}, Y, C}) ->
+    {Z, AA} = out_impl(A),
+    {Z, {AA, X, {B, Y, C}}}.
 
--spec lft(tree_node(Item)) -> maybe_tree_node(Item).
-lft({_, Lft, _}) -> Lft.
-
--spec lft(tree_node(Item), maybe_tree_node(Item)) -> tree_node(Item).
-lft(Node, Lft) -> setelement(2, Node, Lft).
-
--spec rgt(tree_node(Item)) -> maybe_tree_node(Item).
-rgt({_, _, Rgt}) -> Rgt.
-
--spec rgt(tree_node(Item), maybe_tree_node(Item)) -> tree_node(Item).
-rgt(Node, Rgt) -> setelement(3, Node, Rgt).
-
--spec lft_rgt(tree_node(Item), maybe_tree_node(Item), maybe_tree_node(Item)) -> tree_node(Item).
-lft_rgt(Node, Lft, Rgt) -> {item(Node), Lft, Rgt}.
-
--spec rgt_lft(tree_node(Item), maybe_tree_node(Item), maybe_tree_node(Item)) -> tree_node(Item).
-rgt_lft(Node, Rgt, Lft) -> {item(Node), Lft, Rgt}.
-
--spec leaf(Item) -> tree_node(Item).
-leaf(Item) -> {Item, nil, nil}.
-
--spec path_to_node(Item, maybe_tree_node(Item), [{direction(),tree_node(Item)}]) -> [{direction(),tree_node(Item)}].
-path_to_node(_Item, nil, Path) -> Path;
-path_to_node(Item, Node, Path) ->
-    case item(Node) of
-        I when Item =< I -> path_to_node(Item, lft(Node), [{lft,Node}|Path]);
-        I when Item  > I -> path_to_node(Item, rgt(Node), [{rgt,Node}|Path])
-    end.
-
--spec out(tree_node(Item), [tree_node(Item)]) -> {{value, Item}, tree_node(Item)}.
-out({Item, nil, Rgt}, [Node | Path]) -> {{value, Item}, splay_lft(lft(Node,Rgt), Path)};
-out(Node,             Path)          -> out(lft(Node), [Node|Path]).
-
--spec splay_lft(tree_node(Item), [tree_node(Item)]) -> tree_node(Item).
-splay_lft(X, [])            -> X;
-splay_lft(X, [P])           -> rgt(X, lft(P, rgt(X))); % zig
-splay_lft(X, [P, G | Path]) -> splay_lft(rgt(X, rgt_lft(P, lft(G, rgt(P)), rgt(X))), Path). % zig-zig
-
--spec splay(tree_node(Item), [{direction(),tree_node(Item)}]) -> tree_node(Item).
-splay(X, []) -> X;
-splay(X, [{Dir, P}]) ->                % zig
-    case Dir of
-        lft -> rgt(X, lft(P, rgt(X)));
-        rgt -> lft(X, rgt(P, lft(X)))
-    end;
-splay(X, [{Dir,P}, {Dir,G} | Path]) -> % zig-zig
-    splay(case Dir of
-              lft -> rgt(X, rgt_lft(P, lft(G, rgt(P)), rgt(X)));
-              rgt -> lft(X, lft_rgt(P, rgt(G, lft(P)), lft(X)))
-          end,
-          Path);
-splay(X, [{Dir,P}, {_,G} | Path]) ->   % zig-zag
-    splay(case Dir of
-              lft -> rgt_lft(X, lft(P, rgt(X)), rgt(G, lft(X)));
-              rgt -> lft_rgt(X, rgt(P, lft(X)), lft(G, rgt(X)))
-          end,
-          Path).
-
--spec fold_node(heaps:fold_fun(Item), maybe_tree_node(Item), term()) -> term().
-fold_node(_Fun, nil, Acc)             -> Acc;
-fold_node(Fun, {Item, Lft, Rgt}, Acc) -> fold_node(Fun, Rgt, Fun(Item, fold_node(Fun, Lft, Acc))).
+-spec partition(Item, heap(Item)) -> {heap(Item), heap(Item)}.
+partition(_Pivot, empty)                                 -> {empty, empty};
+partition(Pivot, {A, X, B}) when X < Pivot,  B =:= empty -> {{A, X, B}, empty};
+partition(Pivot, {A, X, B}) when Pivot =< X, A =:= empty -> {empty, {A, X, B}};
+partition(Pivot, {A, X, {B1, Y, B2}}) when Y < Pivot ->
+    {Small, Big} = partition(Pivot, B2),
+    {{{A, X, B1}, Y, Small}, Big};
+partition(Pivot, {A, X, {B1, Y, B2}}) when X < Pivot, Pivot =< Y->
+    {Small, Big} = partition(Pivot, B1),
+    {{A, X, Small}, {Big, Y, B2}};
+partition(Pivot, {{A1, Y, A2}, X, B}) when Y < Pivot, Pivot =< X ->
+    {Small, Big} = partition(Pivot, A2),
+    {{A1, Y, Small}, {Big, X, B}};
+partition(Pivot, {{A1, Y, A2}, X, B}) when Pivot =< Y ->
+    {Small, Big} = partition(Pivot, A1),
+    {Small, {Big, Y, {A2, X, B}}}.
